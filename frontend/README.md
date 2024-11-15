a7a106260e43fb0b4fc2a675a6e669b2c00b9d49
abhilash <abhimadaparambath@gmail.com>
2024-11-15 17:09:36 +0530

    homepage-desktopview-completed

--- frontend/README.md
+++ frontend/README.md
@@ -6,3 +6,12 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+
+
+
+https://www.youtube.com/watch?v=eRTTlS0zaW8
+
+https://www.figma.com/design/ZLkjwG5ehxNRrC4SUA2WG7/Prescripto---UI-Design?node-id=0-1&node-type=canvas&t=e4VNIjIztqsoPvd6-0
+
+https://prescripto.vercel.app/
--- frontend/src/pages/User/HomePage.jsx
+++ frontend/src/pages/User/HomePage.jsx
@@ -5,22 +5,25 @@ import header_img from "../../assets/assets_frontend/header_img.png";
 import arrow_icon from "../../assets/assets_frontend/arrow_icon.svg";
 import { Link } from "react-router-dom";
 import General_physician from "../../assets/assets_frontend/General_physician.svg";
+import appointment from "../../assets/assets_frontend/appointment_img.png";
 import doc1 from "../../assets/assets_frontend/doc1.png";
 function HomePage() {
   return (
-    <div className="home-container">
-      <nav className="flex justify-between items-center border-b-[1px] mx-[10%] border-[#ADADAD]  text-[14px] flex-wrap ">
-        <img src={logo} alt="navlogo" className="w-44 my-5" />
-        <ul className="flex space-x-5  font-poppins font-medium">
-          <li>HOME</li>
-          <li>ALL DOCTORS</li>
-          <li>ABOUT</li>
-          <li>CONTACT</li>
-        </ul>
-        <Link className="font-outfit bg-[#5F6FFF] text-[#FFFFFF] px-8 py-3 rounded-full font-[300]">
-          Create account
-        </Link>
-      </nav>
+    <div className="home-container ">
+      <header>
+        <nav className="flex justify-between items-center border-b-[1px]  border-[#ADADAD]  text-[14px] flex-wrap mx-[10%]">
+          <img src={logo} alt="navlogo" className="w-44 my-5" />
+          <ul className="flex space-x-5  font-poppins font-medium">
+            <li>HOME</li>
+            <li>ALL DOCTORS</li>
+            <li>ABOUT</li>
+            <li>CONTACT</li>
+          </ul>
+          <Link className="font-outfit bg-[#5F6FFF] text-[#FFFFFF] px-8 py-3 rounded-full font-[300]">
+            Create account
+          </Link>
+        </nav>
+      </header>
 
       <section className="book-appointment bg-[#5F6FFF] mx-[10%] mt-5 flex px-20  rounded-lg">
         <div className=" pb-24">
@@ -90,30 +93,148 @@ function HomePage() {
         </div>
       </section>
 
-      <section className="total-doctor-container font-outfit my-16">
-        <div className="flex flex-col items-center ">
-          <h2 className="text-3xl font-medium">Top Doctors to Book</h2>
-          <p className="text-center">
+      <section className="total-doctor-container font-outfit mt-32 mx-[10%]">
+        <div className="flex flex-col items-center gap-3 pb-10">
+          <h2 className="text-3xl font-medium ">Top Doctors to Book</h2>
+          <p className="text-center text-sm">
             Simply browse through our extensive list of trusted doctors.
           </p>
         </div>
 
-        <div className="doctor-card-container grid ">
-          <div>
-            <img src={doc1} alt="" />
-            <div>
-              <p></p>
-              <p>Available</p>
+        <div className="doctor-card-container ">
+          <div className="border-[1px] border-[#C9D8FF] rounded-xl ">
+            <div className="bg-[#EAEFFF] border rounded-t-xl">
+              <img src={doc1} alt="" />
             </div>
-            <p>Dr. Richard James</p>
-            <p>General physician</p>
+            <div className="p-4">
+              <div className="flex items-center gap-2">
+                <p className="w-2 h-2 bg-[#0FBF00] rounded-full "></p>
+                <p className="text-[#0FBF00] text-sm">Available</p>
+              </div>
+
+              <p className="text-lg font-medium">Dr. Richard James</p>
+              <p className="text-[#4B5563] font-light text-sm">
+                General physician
+              </p>
+            </div>
+          </div>
+
+          <div className="border-[1px] border-[#C9D8FF] rounded-xl ">
+            <div className="bg-[#EAEFFF] border rounded-t-xl">
+              <img src={doc1} alt="" />
+            </div>
+            <div className="p-4">
+              <div className="flex items-center gap-2">
+                <p className="w-2 h-2 bg-[#0FBF00] rounded-full "></p>
+                <p className="text-[#0FBF00] text-sm">Available</p>
+              </div>
+
+              <p className="text-lg font-medium">Dr. Richard James</p>
+              <p className="text-[#4B5563] font-light text-sm">
+                General physician
+              </p>
+            </div>
+          </div>
+          <div className="border-[1px] border-[#C9D8FF] rounded-xl ">
+            <div className="bg-[#EAEFFF] border rounded-t-xl">
+              <img src={doc1} alt="" />
+            </div>
+            <div className="p-4">
+              <div className="flex items-center gap-2">
+                <p className="w-2 h-2 bg-[#0FBF00] rounded-full "></p>
+                <p className="text-[#0FBF00] text-sm">Available</p>
+              </div>
+
+              <p className="text-lg font-medium">Dr. Richard James</p>
+              <p className="text-[#4B5563] font-light text-sm">
+                General physician
+              </p>
+            </div>
+          </div>
+          <div className="border-[1px] border-[#C9D8FF] rounded-xl ">
+            <div className="bg-[#EAEFFF] border rounded-t-xl">
+              <img src={doc1} alt="" />
+            </div>
+            <div className="p-4">
+              <div className="flex items-center gap-2">
+                <p className="w-2 h-2 bg-[#0FBF00] rounded-full "></p>
+                <p className="text-[#0FBF00] text-sm">Available</p>
+              </div>
+
+              <p className="text-lg font-medium">Dr. Richard James</p>
+              <p className="text-[#4B5563] font-light text-sm">
+                General physician
+              </p>
+            </div>
+          </div>
+          <div className="border-[1px] border-[#C9D8FF] rounded-xl ">
+            <div className="bg-[#EAEFFF] border rounded-t-xl">
+              <img src={doc1} alt="" />
+            </div>
+            <div className="p-4">
+              <div className="flex items-center gap-2">
+                <p className="w-2 h-2 bg-[#0FBF00] rounded-full "></p>
+                <p className="text-[#0FBF00] text-sm">Available</p>
+              </div>
+
+              <p className="text-lg font-medium">Dr. Richard James</p>
+              <p className="text-[#4B5563] font-light text-sm">
+                General physician
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center mt-14 mb-24">
+          <Link className="bg-[#EAEFFF] py-4 px-16 rounded-full text-[#4B5563] text-lg ">
+            more
+          </Link>
+        </div>
+      </section>
+
+      <section className="create-account bg-[#5F6FFF] mx-[10%] flex justify-between items-end px-16 rounded-xl font-outfitm mb-40 ">
+        <div className="pt-20 pb-28">
+          <div className="text-[#FFFFFF] text-5xl font-bold mb-12">
+            <p className="leading-normal">Book Appointment</p>
+            <p>With 100+ Trusted Doctors</p>
+          </div>
+          <Link className="bg-[#FFFFFF] py-4 px-10 rounded-full  ">
+            Create account
+          </Link>
+        </div>
+        <div className="w-[370px]">
+          <img className="" src={appointment} alt="" />
+        </div>
+      </section>
+
+      <footer className="mx-[10%] font-outfit text-[#4B5563]">
+        <div className="company-contact-footer grid grid-cols-[3fr_1fr_1fr] gap-4 leading-8 justify-self-center">
+          <div>
+            <img className="w-[190px] mb-5" src={logo} alt="logo" />
+            <p className="text-start pr-36">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          </div>
+          <div>
+            <p className="mb-5 font-medium text-xl text-black pt-3">COMPANY</p>
+            <ul>
+              <li>Home</li>
+              <li>About us</li>
+              <li>Contact us</li>
+              <li>Privacy policy</li>
+            </ul>
+          </div>
+          <div>
+            <p className="mb-5 font-medium text-xl  text-black pt-3">GET IN TOUCH</p>
+            <ul>
+              <li>+1-212-456-7890</li>
+              <li>greatstackdev@gmail.com</li>
+            </ul>
           </div>
         </div>
 
-        <Link>more</Link>
-      </section>
+        <div className="company-copyright-footer border-t border-[#BDBDBD] mt-10 p-5 text-black">
+          <p className="text-center">Copyright © 2024 GreatStack - All Right Reserved.</p>
+        </div>
+      </footer>
 
-      <p>hi</p>
     </div>
   );
 }
